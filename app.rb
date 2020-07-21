a1ef675e09c41dfc5dae5001201e5911a38bb675
Mocadi-dot <59927623+mocadi-dot@users.noreply.github.com>
2020-07-21 23:49:17 +0200

    Add files via upload

--- app.rb
+++ app.rb
@@ -1,7 +1,32 @@
 require 'bundler'
+require 'pry'
 Bundler.require
 
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-binding.pry
+player1 = Player.new("The Dark Knight")
+player2 = Player.new("Princess Zelia")
+
+while player1.life_points > 0 && player2.life_points >0 do
+  puts "
+ -----------
+ "
+
+ puts "Voici l'état des combattants :"
+ player1.show_state
+ player2.show_state
+
+ puts "
+ ---
+ "
+
+ puts "Luttez ou mourrez :"
+
+ player1.attacks(player2) # player 1 attaque
+ if player2.life_points <= 0 # pas de mort-vivants
+   break
+ end
+ player2.attacks(player1) # player 2 attaque
+
+end
