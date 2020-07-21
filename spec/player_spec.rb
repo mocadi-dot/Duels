7faa3114efa3b8d1ff0c8a74c5bc7c96902b8675
Mocadi-dot <59927623+mocadi-dot@users.noreply.github.com>
2020-07-21 21:39:45 +0200

    Add files via upload

---
+++ spec/player_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/player'
+
+#describe "Test class and method for Class Player" do
+
+  #describe "***" do (...) end pour introduire un groupe de test, généralement axé sur une méthode donnée
+#it "***" do (...) end pour introduire un test précis au sein de ce groupe, généralement axé sur une fonctionnalité donnée
+#expect(lmethode_ou_variable_ou_autre).to eq(resultat_attendu) pour faire le test qu'une méthode ou variable ou autre donne le résultat attendu
